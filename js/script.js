16f55af84b32290295dba7d87402161b3dd3afea
DanieleSalinitro <daniele.salinitro@icloud.com>
2024-04-04 11:33:49 +0200

    modifiche

--- js/script.js
+++ js/script.js
@@ -1,17 +1,37 @@
 import {contacts} from './data.js';
 
+const dateTime = luxon.DateTime;
+
 const { createApp } = Vue;
 
     const app = createApp({
       data(){
         return {
             contacts,
-            activeContactId: 1
+            activeContactId: 1,
+            messageText: ''
         }
       },
       methods: {
         setActiveContactId(id){
-          this.setActiveContactId = id
+          this.activeContactId = id
+        }, 
+        CreateMessage(msg, status){
+          const newMessage = {
+            date: dateTime.now().setLocale('it').toFormat('dd/MM/yyyy HH:mm:ss'),
+            message: msg,
+            status: status
+          }
+          return newMessage;
+        }, 
+        sendMessage(){
+          const newMessage = this.CreateMessage(this.messageText, 'sent');
+          this.activeContact.messages.push(newMessage);
+          this.messageText = '';
+          setTimeout(() => {
+            const newMessage = this.CreateMessage('ok', 'received');
+            this.activeContact.messages.push(newMessage)
+          }, 1000);
         }
       }, 
       computed: {
